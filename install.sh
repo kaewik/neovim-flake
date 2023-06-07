60465dff0cdbef542ab5a4d3ff51ca85b9bdfbe8
Lukas Zenner <lukas.zenner@goto.com>
2023-06-07 11:16:00 +0200

    Fix install script

--- install.sh
+++ install.sh
@@ -1,7 +1,4 @@
 #!/bin/bash -ex
 package_indices=$(nix profile list | grep neovim-flake | cut -d ' ' -f 1)
-echo $package_indices
-for idx in $package_indices; do
-    nix profile remove $idx
-done
+nix profile remove $package_indices
 nix profile install .#
