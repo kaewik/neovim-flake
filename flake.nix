86d9655425d2d1a1116e737f7e2cd07c7e2c9293
lugggas <lukas.zenner94@googlemail.com>
2023-09-29 16:06:15 +0200

    Enable all systems

--- flake.nix
+++ flake.nix
@@ -1,33 +1,41 @@
 {
   inputs.neovim-flake.url = "github:jordanisaacs/neovim-flake";
 
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
   inputs.copilot.url = "github:github/copilot.vim/release";
   inputs.copilot.flake = false;
 
   outputs = {
+    self,
+    flake-utils,
     nixpkgs,
     neovim-flake,
     copilot,
     ...
-  }: let
-    system = "x86_64-linux";
-    pkgs = nixpkgs.legacyPackages.${system};
-    configModule = {
-    # Add any custom options (and feel free to upstream them!)
-    # options = ...
+  }: flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        configModule = {
+          # Add any custom options (and feel free to upstream them!)
+          # options = ...
+          config.build.rawPlugins = {
+            copilot.src = copilot;
+          };
+          config.vim.theme.enable = true;
+          config.vim.startPlugins = ["copilot"];
+        };
 
-      config.build.rawPlugins = {
-        copilot.src = copilot;
-      };
-      config.vim.theme.enable = true;
-      config.vim.startPlugins = ["copilot"];
-    };
-
-    customNeovim = neovim-flake.lib.neovimConfiguration {
-      modules = [configModule];
-      inherit pkgs;
-    };
-  in {
-    packages.${system}.neovim = customNeovim;
-  };
+      in {
+        packages = rec {
+          customNeovim = neovim-flake.lib.neovimConfiguration {
+            modules = [configModule];
+            inherit pkgs;
+          };
+          neovim = customNeovim;
+        };
+        apps = rec {
+          neovim = flake-utils.lib.mkApp { drv = self.packages.${system}.neovim; };
+        };
+      });
 }
