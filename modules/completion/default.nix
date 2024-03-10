a6f4476273462987cdd03862bd75a9ae5ea378c5
kaewik <32685131+kaewik@users.noreply.github.com>
2024-03-10 14:29:22 +0100

    Fix: tab completion on :

--- modules/completion/default.nix
+++ modules/completion/default.nix
@@ -181,17 +181,14 @@ in
           end,
         ''}
       })
-      cmp.setup.cmdline('/', {
+      cmp.setup.cmdline({'/', '?'}, {
         mapping = cmp.mapping.preset.cmdline(),
         sources = {
           { name = 'buffer' }
         }
       })
       cmp.setup.cmdline(':', {
-        mapping = cmp.mapping.preset.cmdline(),
         sources = cmp.config.sources({
-            { name = 'path' }
-        }, {
             { name = 'cmdline' }
         })
       })
